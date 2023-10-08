4e55c204b70a12f2d579ad7a4ddc9c59402c603e
Dustin Quam <quamsta@gmail.com>
2023-10-08 11:29:58 -0500

    added case for no date being available

--- components/FoldingDate.js
+++ components/FoldingDate.js
@@ -1,13 +1,22 @@
-import { formatRelative, format } from 'date-fns';
-import dynamic from 'next/dynamic'
+import { formatRelative, format } from "date-fns";
+import dynamic from "next/dynamic";
 
 export default function FoldingDate({ dateString }) {
-    // Hacky way to force a UTC timezone on the date we get from Folding@Home's API, which
-    // doesn't contain UTC timezone information baked in, but it is definitely UTC.
-    const formattedTimestamp = dateString.replace(' ', 'T').concat('Z')
-    const date = new Date(formattedTimestamp);
-    const now = new Date;
+  // If there's no date in the API response (e.g., we haven't folded anything in a while)
+  // Return empty string.
+  if (dateString === undefined) {
+    return "No recent work unit.";
+  }
 
-    return <time dateTime={dateString} suppressHydrationWarning>{formatRelative(date, now)} {format(date, 'z')}</time>;
+  // Hacky way to force a UTC timezone on the date we get from Folding@Home's API, which
+  // doesn't contain UTC timezone information baked in, but it is definitely UTC.
+  const formattedTimestamp = dateString.replace(" ", "T").concat("Z");
+  const date = new Date(formattedTimestamp);
+  const now = new Date();
 
+  return (
+    <time dateTime={dateString} suppressHydrationWarning>
+      {formatRelative(date, now)} {format(date, "z")}
+    </time>
+  );
 }
--- components/FoldingStatsBox.js
+++ components/FoldingStatsBox.js
@@ -1,29 +1,34 @@
-import Box from './Box'
-import boxStyles from './Box.module.css'
-import FoldingDate from './FoldingDate';
-import Link from 'next/link';
+import Box from "./Box";
+import boxStyles from "./Box.module.css";
+import FoldingDate from "./FoldingDate";
+import Link from "next/link";
 
 export default function FoldingStatsBox({ foldingStats }) {
-    return (
-        <Box>
-            <h2 className={`${boxStyles.boxHeading}`}>My <a href="https://foldingathome.org">Folding@Home</a> Stats</h2>
-            <div className={boxStyles.boxContent}>
-                <p className="mt-0">
-                    <strong>Username:</strong> <a href={"https://stats.foldingathome.org/donor/" + foldingStats.name}>{foldingStats.name}</a> <br />
-                    <strong>Score:</strong> {foldingStats.score.toLocaleString()} <br />
-                    <strong>Rank:</strong> {foldingStats.rank.toLocaleString()} <br />
-                    <strong>Last work unit uploaded:</strong> <FoldingDate dateString={foldingStats.last} /><br />
-                    <strong>Number of active clients:</strong> {foldingStats.active_7}
-                </p>
-                <p className="my-0">
-                    <Link href="/stats">Full statistics &rarr;</Link>
-                </p>
-            </div>
-
-        </Box>
-    )
+  return (
+    <Box>
+      <h2 className={`${boxStyles.boxHeading}`}>
+        My <a href="https://foldingathome.org">Folding@Home</a> Stats
+      </h2>
+      <div className={boxStyles.boxContent}>
+        <p className="mt-0">
+          <strong>Username:</strong>{" "}
+          <a
+            href={"https://stats.foldingathome.org/donor/" + foldingStats.name}
+          >
+            {foldingStats.name}
+          </a>{" "}
+          <br />
+          <strong>Score:</strong> {foldingStats.score.toLocaleString()} <br />
+          <strong>Rank:</strong> {foldingStats.rank.toLocaleString()} <br />
+          <strong>Last work unit uploaded:</strong>{" "}
+          <FoldingDate dateString={foldingStats.last} />
+          <br />
+          <strong>Number of active clients:</strong> {foldingStats.active_7}
+        </p>
+        <p className="my-0">
+          <Link href="/stats">Full statistics &rarr;</Link>
+        </p>
+      </div>
+    </Box>
+  );
 }
-
-
-
-
